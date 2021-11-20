d63e64ceb9b7a6fb84a04ba0f473bcda1cac16ad
Moumen Soliman <moumensoliman@gmail.com>
2021-11-21 00:07:54 +0200

    Sec 3 exercise

---
+++ js/index.js
@@ -0,0 +1,46 @@
+var userInfo = {
+    name: "Moumen Soliman",
+    username: "moumen-soliman",
+    bio: "Frontend Engineer",
+    image: "https://avatars.githubusercontent.com/u/24474287?v=4"
+};
+
+var followersHandler = {
+    follower: 77,
+    following: 234,
+    starsCount: 149
+};
+
+var userMainInfo = {
+    location: "Egypt",
+    email: "moumensoliman@gmail.com",
+    link: {
+        title: "Github Link",
+        href: "#"
+    }
+};
+
+var organization = [
+    {
+        name: "baims",
+        private: true,
+        image: "https://avatars.githubusercontent.com/u/8783891?s=64&v=4"
+    }
+];
+
+var reposArr = [
+    {
+        name: "repo-name",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae cupiditate totam similique iusto, quo aperiam odit animi fugiat harum nostrum officia eveniet nemo explicabo, ex blanditiis optio, in est ab.",
+        language: "JavaScript",
+        updateTime: "Updated 1 hour ago",
+        public: true,
+        topics: [{
+            name: "JavaScript",
+            link: "#"
+        },{
+            name: "React",
+            link: "#"
+        }]
+    }
+]
