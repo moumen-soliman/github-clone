8ec3cd7ef44bdba054d12d1e3204ca4a50fd0e8c
Moumen Soliman <moumensoliman@gmail.com>
2021-12-03 00:59:28 +0200

    Create datatypes.js

---
+++ js/sec5/datatypes.js
@@ -0,0 +1,59 @@
+// Objects
+
+/* const car = {
+    name: 'volvo',
+    color: 'red',
+    model: '2007',
+    'wheel': 17,
+    isNew: true,
+    carBody: {
+        rolling: true,
+        flexible: true,
+    },
+    sayWelcome: (param1, param2) => `${param1} say ${param2}`
+}
+
+const newCar = Object.create(car)
+
+newCar.newName = 'toyota';
+
+const target = { a: 1, b: 2 };
+const source = { b: 3, c: 4 }
+
+Object.assign(target, source)
+
+
+Object.defineProperty(car, 'noUpdateColor', {
+    value: 'yellow',
+    writable: false
+}) */
+
+// Reference vs Values
+
+/* var car = 'volvo';
+
+var car2 = 'toyota';
+
+car2 = 'hello'
+
+car = car2 
+
+var object1 = {
+    first: "moumen",
+    second: "soliman"
+}
+
+var object2 = {
+    welcomeWord: "hello"
+}
+
+var object3 = {
+    ...object1,
+    ...object2
+} */
+
+// Array
+
+var animals = ["poppy", "dog", "cat", "bird"];
+
+var calcArr = [1 , 20 , 5 ,400 ,600]
